1fa1dd9bdf16ca9bb5b4b16b4b678781be11f284
AytajMammadova306 <aytajamam@code.edu.az>
2025-07-22 11:34:52 +0400

    initial

---
+++ fortasks/forexample/Program.cs
@@ -0,0 +1,45 @@
+﻿#region task1
+//Console.WriteLine("Zehmet olmasa ilk herfi böyük olmaqla daxil edin.");
+//string month = Console.ReadLine();
+//switch (month)
+//{
+//    case "Yanvar" or "Mart" or "May" or "Iyul" or "Avqust" or "Oktyabr" or "Dekabr":
+//        Console.WriteLine(31);
+//        break;
+//    case "Aprel" or "Iyun" or "Sentyabr" or "Noyabr":
+//        Console.WriteLine(30);
+//        break;
+//    case "Fevral":
+//        Console.WriteLine(28);
+//        break;
+//} 
+#endregion
+
+#region task2
+//int sum = 0;
+//for (int i = 0; i < 101; i+=2)
+//{
+//    sum +=i;
+//}
+//Console.WriteLine(sum); 
+#endregion
+
+#region task3
+//int num = int.Parse(Console.ReadLine());
+//int count = 0;
+//for (int i = 1; i <= num; i++)
+//{
+//    if (num % i == 0)
+//    {
+//        count++;
+//    }
+//}
+//if (count == 2)
+//{
+//    Console.WriteLine("sade");
+//}
+//else
+//{
+//    Console.WriteLine("murekkeb");
+//} 
+#endregion
